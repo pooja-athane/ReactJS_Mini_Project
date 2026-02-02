7db54e6366b95518b48efee76940a4a4ac84dc02
Pooja <athanepooja313@gmail.com>
2026-02-02 11:43:14 +0530

    added about section

--- Portfolio_Website/src/section/About.jsx
+++ Portfolio_Website/src/section/About.jsx
@@ -1,8 +1,11 @@
 import React, { useRef } from 'react'
 import Card from '../components/Card'
+import { Globe } from '../components/Globe'
+import CopyEmailButton from '../components/CopyEmailButton'
+import { Frameworks } from '../components/Frameworks'
 
 const About = () => {
-    const grid2Container =useRef()
+    const grid2Container = useRef()
 
     const cards = [
         {
@@ -67,7 +70,7 @@ const About = () => {
                         <p className=' flex items-end text-5xl text-gray-50'>
                             CODE IS CRAFT
                         </p>
-                       {/* <Card
+                        {/* <Card
                             style={{ rotate: "75deg", top: "30%", left: "20%" }}
                             text="GRASP"
                             containerRef={grid2Container}
@@ -120,10 +123,48 @@ const About = () => {
                 </div>
                 {/* ----------- */}
 
+                {/* card 3 */}
+                <div className="grid-default-color grid-3">
+                    <div className="z-10 w-[50%]">
+                        <p className="headtext">Time Zone</p>
+                        <p className="subtext">
+                            I'm based in Mars, and open to remote work worldwide
+                        </p>
+                    </div>
+                    <figure className='absolute left-[30%] top-[10%]'>
+                        <Globe />
+                    </figure>
+                </div>
 
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
+                {/* --------- */}
+                {/* car 4 */}
+                <div className="grid-default-color grid-4">
+                    <div className="flex flex-col items-center justify-center gap-4 size-full">
+                        <p className="text-center headtext">
+                            Do you want to start a project together?
+                        </p>
+                        <CopyEmailButton />
+                    </div>
+                </div>
+
+                {/* ----------- */}
+
+                {/* card 5 */}
+
+                <div className="grid-default-color grid-5">
+                    <div className="z-10 w-[50%]">
+                        <p className="headText">Teck Stack</p>
+                        <p className="subtext">
+                            I specialize in a variety of languages, frameworks, and tools taht
+                            allow me to build robust and scalable applications
+                        </p>
+                    </div>
+                    <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+                        <Frameworks />
+                    </div>
+                </div>
+
+                {/* ---------- */}
             </div>
         </div>
     )
