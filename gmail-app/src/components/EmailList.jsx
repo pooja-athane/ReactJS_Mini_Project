3c380a827d1db361e8428f23fee43a37eda68b4b
Pooja <athanepooja313@gmail.com>
2026-02-20 18:51:32 +0530

    adding seperate emails.js file for well file structure.

--- gmail-app/src/components/EmailList.jsx
+++ gmail-app/src/components/EmailList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import EmailRow from './EmailRow';
+import { emails } from '../constants/emails';
 
 function EmailList(
   {
@@ -9,135 +10,7 @@ function EmailList(
     sentEmails
   }
 ) {
-  const emails = [
-    {
-      id: 1,
-      sender: "Amazon",
-      subject: "Your order has been shipped",
-      message: "Your package will arrive tomorrow.",
-      time: "10:30 AM"
-    },
-    {
-      id: 2,
-      sender: "Flipkart",
-      subject: "Big Sale is Live",
-      message: "Don't miss amazing offers today.",
-      time: "9:15 AM"
-    },
-    {
-      id: 3,
-      sender: "LinkedIn",
-      subject: "New Job Alert",
-      message: "React Developer job posted.",
-      time: "8:40 AM"
-    },
-    {
-      id: 4,
-      sender: "Instagram",
-      subject: "New Follower",
-      message: "Someone started following you.",
-      time: "Yesterday"
-    },
-    {
-      id: 5,
-      sender: "Swiggy",
-      subject: "Food Delivered",
-      message: "Your order has been delivered.",
-      time: "Yesterday"
-    },
-    {
-      id: 6,
-      sender: "Paytm",
-      subject: "Cashback Received",
-      message: "You received ₹50 cashback.",
-      time: "Mon"
-    },
-    {
-      id: 7,
-      sender: "GitHub",
-      subject: "New Pull Request",
-      message: "Someone opened a PR in your repo.",
-      time: "Mon"
-    },
-    {
-      id: 8,
-      sender: "YouTube",
-      subject: "New Video Uploaded",
-      message: "Your subscribed channel uploaded a video.",
-      time: "Sun"
-    },
-    {
-      id: 9,
-      sender: "Zomato",
-      subject: "Flat 60% OFF",
-      message: "Order now and save big.",
-      time: "Sun"
-    },
-    {
-      id: 10,
-      sender: "Twitter",
-      subject: "New Mention",
-      message: "Someone mentioned you in a tweet.",
-      time: "Sat"
-    },
-    {
-      id: 11,
-      sender: "Amazon",
-      subject: "Your order has been shipped",
-      message: "Your package will arrive tomorrow.",
-      time: "10:30 AM"
-    },
-    {
-      id: 12,
-      sender: "Flipkart",
-      subject: "Big Sale is Live",
-      message: "Don't miss amazing offers today.",
-      time: "9:15 AM"
-    },
-    {
-      id: 13,
-      sender: "LinkedIn",
-      subject: "New Job Alert",
-      message: "React Developer job posted.",
-      time: "8:40 AM"
-    },
-    {
-      id: 14,
-      sender: "Instagram",
-      subject: "New Follower",
-      message: "Someone started following you.",
-      time: "Yesterday"
-    },
-    {
-      id: 15,
-      sender: "Swiggy",
-      subject: "Food Delivered",
-      message: "Your order has been delivered.",
-      time: "Yesterday"
-    },
-    {
-      id: 16,
-      sender: "Amazon",
-      subject: "Your order has been shipped",
-      message: "Your package will arrive tomorrow.",
-      time: "10:30 AM"
-    },
-    {
-      id: 17,
-      sender: "Flipkart",
-      subject: "Big Sale is Live",
-      message: "Don't miss amazing offers today.",
-      time: "9:15 AM"
-    },
-    {
-      id: 18,
-      sender: "Instagram",
-      subject: "New Follower",
-      message: "Someone started following you.",
-      time: "Yesterday"
-    },
-  ];
-
+  
   const displayEmailornot = activeSection === "Sent" ? sentEmails : emails;
 
   if (selectedEmail) {
